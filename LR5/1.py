829730d9d109fd6dd88954bd7e347a2945ffc3df
Evgeshhha <129299012+Evgeshhha@users.noreply.github.com>
2025-01-12 21:21:46 +0300

    Update 1.py

--- LR5/1.py
+++ LR5/1.py
@@ -3,11 +3,6 @@ class Book:
     author = "Садовникова"
     year = "2024"
 
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
-
     def get_info(self):
         return f"Название книги: {self.title}, Автор: {self.author}, Год издания: {self.year}"
     
